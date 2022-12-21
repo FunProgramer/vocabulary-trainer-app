ea327086135729cedd0cf9953366d7bee0742f31
FunProgramer <47451400+FunProgramer@users.noreply.github.com>
2022-12-21 15:41:44 +0100

    Commit everything

---
+++ lib/config.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+//#00b73d
+const Map<int, Color> _colorMap =
+{
+  50: Color.fromRGBO(0, 183, 61, .1), //Merrrrrlin der hacker
+  100: Color.fromRGBO(0, 183, 61, .2),
+  200: Color.fromRGBO(0, 183, 61, .3),
+  300: Color.fromRGBO(0, 183, 61, .4),
+  400: Color.fromRGBO(0, 183, 61, .5),
+  500: Color.fromRGBO(0, 183, 61, .6),
+  600: Color.fromRGBO(0, 183, 61, .7),
+  700: Color.fromRGBO(0, 183, 61, .8),
+  800: Color.fromRGBO(0, 183, 61, .9),
+  900: Color.fromRGBO(0, 183, 61, 1),
+};
+
+const MaterialColor primaryColor = MaterialColor(0x00B73D, _colorMap);
